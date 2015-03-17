69732e7a64e6dab08552d271978beeb2a0d2ec53
Nicoaedhel <nicoa.cn@gmail.com>
2015-03-17 19:59:48 +0100

    Ajout signal sur Movie
    Ajout du signal 404 sur movie.php pour movie_view

--- core/rsc/movie.php
+++ core/rsc/movie.php
@@ -45,33 +45,40 @@ function _movies_list(){
 
 function _movie_view($id){
 	$ini_array = parse_ini_file('depot.ini', true);
+	if (file_exists('depot/movie/'.$id.'.json')) {
 
-	// Identifiant resource
-	$result['_api_rsc']['_name'] = 'movie';
-	$result['_api_rsc']['_id'] = $id;
-	$result['_api_rsc']['_depot'] = $ini_array['DEPOT']['local'];
+		// Identifiant resource
+		$result['_api_rsc']['_name'] = 'movie';
+		$result['_api_rsc']['_id'] = $id;
+		$result['_api_rsc']['_depot'] = $ini_array['DEPOT']['local'];
 
-	// Lien api
-	$result['_api_link']['_resolver']['_list']['_url'] = $ini_array['DEPOT']['local'].'resolver/movies/';
-	$result['_api_link']['_resolver']['_list']['_method'] = 'GET';
-	$result['_api_link']['_list']['_url'] = $ini_array['DEPOT']['local'].'movies/';
-	$result['_api_link']['_list']['_method'] = 'GET';
-	$result['_api_link']['_view']['_url'] = $ini_array['DEPOT']['local'].'movie/'.$id;
-	$result['_api_link']['_view']['_method'] = 'GET';
-	$result['_api_link']['_add']['_url'] = $ini_array['DEPOT']['local'].'movie/';
-	$result['_api_link']['_add']['_method'] = 'POST';
-	$result['_api_link']['_edit']['_url'] = $ini_array['DEPOT']['local'].'movie/';
-	$result['_api_link']['_edit']['_method'] = 'PUT';
-	$result['_api_link']['_delete']['_url'] = $ini_array['DEPOT']['local'].'movie/';
-	$result['_api_link']['_delete']['_method'] = 'DELETE';
+		// Lien api
+		$result['_api_link']['_resolver']['_list']['_url'] = $ini_array['DEPOT']['local'].'resolver/movies/';
+		$result['_api_link']['_resolver']['_list']['_method'] = 'GET';
+		$result['_api_link']['_list']['_url'] = $ini_array['DEPOT']['local'].'movies/';
+		$result['_api_link']['_list']['_method'] = 'GET';
+		$result['_api_link']['_view']['_url'] = $ini_array['DEPOT']['local'].'movie/'.$id;
+		$result['_api_link']['_view']['_method'] = 'GET';
+		$result['_api_link']['_add']['_url'] = $ini_array['DEPOT']['local'].'movie/';
+		$result['_api_link']['_add']['_method'] = 'POST';
+		$result['_api_link']['_edit']['_url'] = $ini_array['DEPOT']['local'].'movie/';
+		$result['_api_link']['_edit']['_method'] = 'PUT';
+		$result['_api_link']['_delete']['_url'] = $ini_array['DEPOT']['local'].'movie/';
+		$result['_api_link']['_delete']['_method'] = 'DELETE';
 
-	// Traitement resource
-	$json = file_get_contents("depot/movie/$id.json");
-	$result1 = json_decode($json, true);
-	$system = new System();
-	$result1 = $system->_filter_json($result1); // Filtre json
-	$result = array_merge($result, $result1);
-	echo json_encode($result); // Envoi de la réponse
+		// Traitement resource
+		$json = file_get_contents("depot/movie/$id.json");
+		$result1 = json_decode($json, true);
+		$system = new System();
+		$result1 = $system->_filter_json($result1); // Filtre json
+		$result = array_merge($result, $result1);
+		echo json_encode($result); // Envoi de la réponse
+
+	} else {
+	    $app = \Slim\Slim::getInstance();
+	    $app->halt(404);
+	}
+	exit;	
 }
 
 function _movie_add(){
