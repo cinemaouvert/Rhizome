89ae864c59ed6d4776a340f65256245451fd71a5
Nicoaedhel <nicoa.cn@gmail.com>
2015-03-17 18:26:45 +0100

    Ajout de la ressource Attachment
    Ajout ressource attachment. Avec deux images dans Attachment pour
    l’exemple.

--- core/rsc.php
+++ core/rsc.php
@@ -1,5 +1,6 @@
 <?php 
-	require 'core/rsc/movie.php';      // initialisation de la ressource movies
-	require 'core/rsc/people.php';      // initialisation de la ressource movies
-	require 'core/rsc/depot.php';       // initialisation de la ressource depot
-	require 'core/rsc/resolver.php';       // initialisation de la ressource resolver
+	require 'core/rsc/movie.php';      		// initialisation de la ressource movies
+	require 'core/rsc/people.php';      	// initialisation de la ressource peoples
+	require 'core/rsc/attachment.php';      // initialisation de la ressource attachment
+	require 'core/rsc/depot.php';       	// initialisation de la ressource depot
+	require 'core/rsc/resolver.php';        // initialisation de la ressource resolver
