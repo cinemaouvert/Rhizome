1fd358434bfc53309415fba3a528f1da09de8439
Nicoaedhel <nicoa.cn@gmail.com>
2015-03-17 17:29:19 +0100

    Correction resolver
    Correction du resolver qui ne prenait pas en compte le dépôt local dans
    les résultats de l’api.

--- core/rsc/resolver.php
+++ core/rsc/resolver.php
@@ -17,6 +17,11 @@ function _resolver_movies_all(){
 	// Analyse avec sections de depot.ini .
 	$result = [];
 	$ini_array = parse_ini_file('depot.ini', true);
+	$adress_resolver = $ini_array['DEPOT']['local'];
+	$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
+    $result1 = file_get_contents($adress_resolver.'movies',false,$context);
+    $result1 = json_decode($result1, true);
+    $result = array_merge($result, $result1);
 	foreach($ini_array['RESOLVER HOST'] as $row) {
 		$adress_resolver = $row;
 		$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
@@ -31,6 +36,11 @@ function _resolver_peoples_all(){
 	// Analyse avec sections de depot.ini .
 	$result = [];
 	$ini_array = parse_ini_file('depot.ini', true);
+	$adress_resolver = $ini_array['DEPOT']['local'];
+	$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
+    $result1 = file_get_contents($adress_resolver.'movies',false,$context);
+    $result1 = json_decode($result1, true);
+    $result = array_merge($result, $result1);
 	foreach($ini_array['RESOLVER HOST'] as $row) {
 		$adress_resolver = $row;
 		$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
