2d627e422b7c0648fef2e0623a6f84cc8fe180ff
Nicoaedhel <nicoa.cn@gmail.com>
2015-03-24 12:04:14 +0100

    Route movies/key
    Ajout de la route movie/key pour que l’api serve la ressource movie via
    la clé utilisateur.

--- core/rsc/movie.php
+++ core/rsc/movie.php
@@ -1,6 +1,7 @@
 <?php
 
 $app->get(    '/movies/',                    '_movies_list');         	  // affiche tous les films du depot
+$app->get(    '/movies/:key',                '_movies_list_by_key');      // affiche tous les films du depot par clé
 $app->get(    '/movie/:id',                  '_movie_view');              // affiche un film du depot
 $app->post(   '/movie/',                  	 '_movie_add');               // ajoute un film dans le depot 
 
@@ -21,6 +22,9 @@ function _movies_list(){
 				$result[$i]['_api_rsc']['_name'] = 'movie';
 				$result[$i]['_api_rsc']['_id'] = str_replace('.json', '', $file);
 				$result[$i]['_api_rsc']['_depot'] = $ini_array['DEPOT']['local'];
+
+				//suppression _api_key_password
+				unset($result[$i]['_api_key_password']);
 
 				//lien api
 				$result[$i]['_api_link']['_resolver']['_list']['_url'] = $ini_array['DEPOT']['local'].'resolver/movies/';
@@ -44,6 +48,50 @@ function _movies_list(){
 	echo $system->_filter_json(json_encode($result)); // Envoi de la réponse
 }
 
+function _movies_list_by_key($key){
+	// Analyse avec sections de core/core.ini .
+	$ini_array = parse_ini_file('depot.ini', true);
+	$i = 0;
+	$system = new System();
+	if($dir = opendir('depot/movie')){
+		while(false !== ($file = readdir($dir))){
+			if($file != '.' && $file != '..' && $file != '.DS_Store'){
+				$json = file_get_contents("depot/movie/$file");
+				$result1 = json_decode($json, true);		
+				if(isset($result1['_api_key_user']) AND $result1['_api_key_user'] == $key){
+					$result[$i] = json_decode($json, true);	
+					$result[$i] = $system->_wiki(json_decode($json, true));
+					
+					//identifiant resource
+					$result[$i]['_api_rsc']['_name'] = 'movie';
+					$result[$i]['_api_rsc']['_id'] = str_replace('.json', '', $file);
+					$result[$i]['_api_rsc']['_depot'] = $ini_array['DEPOT']['local'];
+
+					//suppression _api_key_password
+					unset($result[$i]['_api_key_password']);
+
+					//lien api
+					$result[$i]['_api_link']['_resolver']['_list']['_url'] = $ini_array['DEPOT']['local'].'resolver/movies/';
+					$result[$i]['_api_link']['_resolver']['_list']['_method'] = 'GET';
+					$result[$i]['_api_link']['_list']['_url'] = $ini_array['DEPOT']['local'].'movies/';
+					$result[$i]['_api_link']['_list']['_method'] = 'GET';
+					$result[$i]['_api_link']['_view']['_url'] = $ini_array['DEPOT']['local'].'movie/'.$result[$i]['_api_rsc']['_id'];
+					$result[$i]['_api_link']['_view']['_method'] = 'GET';
+					$result[$i]['_api_link']['_add']['_url'] = $ini_array['DEPOT']['local'].'movie/';
+					$result[$i]['_api_link']['_add']['_method'] = 'POST';
+					$result[$i]['_api_link']['_edit']['_url'] = $ini_array['DEPOT']['local'].'movie/';
+					$result[$i]['_api_link']['_edit']['_method'] = 'PUT';
+					$result[$i]['_api_link']['_delete']['_url'] = $ini_array['DEPOT']['local'].'movie/';
+					$result[$i]['_api_link']['_delete']['_method'] = 'DELETE';
+					$i++;
+				}	
+			}
+		}
+	}
+	closedir($dir);
+	echo $system->_filter_json(json_encode($result)); // Envoi de la réponse
+}
+
 function _movie_view($id){
 	$ini_array = parse_ini_file('depot.ini', true);
 	if (file_exists('depot/movie/'.$id.'.json')) {
@@ -52,7 +100,6 @@ function _movie_view($id){
 		$result['_api_rsc']['_name'] = 'movie';
 		$result['_api_rsc']['_id'] = $id;
 		$result['_api_rsc']['_depot'] = $ini_array['DEPOT']['local'];
-		
 
 		// Lien api
 		$result['_api_link']['_resolver']['_list']['_url'] = $ini_array['DEPOT']['local'].'resolver/movies/';
@@ -72,6 +119,10 @@ function _movie_view($id){
 		$system = new System();
 		$json = file_get_contents("depot/movie/$id.json");
 		$result1 = $system->_wiki(json_decode($json, true));
+
+		//suppression _api_key_password
+		unset($result1['_api_key_password']);
+
 		$result = array_merge($result1, $result);
 		echo $system->_filter_json(json_encode($result)); // Envoi de la réponse
 
