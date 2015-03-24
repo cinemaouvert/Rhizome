1a15b8e87d0fe6fc5f81820475a1f5d530c680f1
Nicoaedhel <nicoa.cn@gmail.com>
2015-03-24 14:05:28 +0100

    Ajout de la recherche par clé via resolver
    pour movie et people

--- core/rsc/resolver.php
+++ core/rsc/resolver.php
@@ -2,7 +2,9 @@
 
 $app->get(    '/resolver/depot/',                     '_resolver_depot');              			// affiche tous les depots du resolver
 $app->get(    '/resolver/movies/',                    '_resolver_movies_all');         			// affiche tous les films du resolver
+$app->get(    '/resolver/movies/:key',                '_resolver_movies_all_by_key');           // affiche toutes les films du resolver
 $app->get(    '/resolver/peoples/',                   '_resolver_peoples_all');         		// affiche toutes les personnes du resolver
+$app->get(    '/resolver/peoples/:key',               '_resolver_peoples_all_by_key');          // affiche toutes les personnes du resolver
 
 
 
@@ -50,3 +52,42 @@ function _resolver_peoples_all(){
 	}
 	echo json_encode($result); // Envoi de la réponse
 }
+
+function _resolver_movies_all_by_key($key){
+	// Analyse avec sections de depot.ini .
+	$result = [];
+	$ini_array = parse_ini_file('depot.ini', true);
+	$adress_resolver = $ini_array['DEPOT']['local'];
+	$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
+    $result1 = file_get_contents($adress_resolver.'movies/'.$key,false,$context);
+    $result1 = json_decode($result1, true);
+    $result = array_merge($result, $result1);
+	foreach($ini_array['RESOLVER HOST'] as $row) {
+		$adress_resolver = $row;
+		$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
+	    $result1 = file_get_contents($adress_resolver.'movies/'.$key,false,$context);
+	    $result1 = json_decode($result1, true);
+	    $result = array_merge($result, $result1);
+	}
+	echo json_encode($result); // Envoi de la réponse
+}
+
+function _resolver_peoples_all_by_key($key){
+	// Analyse avec sections de depot.ini .
+	$result = [];
+	$ini_array = parse_ini_file('depot.ini', true);
+	$adress_resolver = $ini_array['DEPOT']['local'];
+	$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
+    $result1 = file_get_contents($adress_resolver.'peoples/'.$key,false,$context);
+    $result1 = json_decode($result1, true);
+    $result = array_merge($result, $result1);
+	foreach($ini_array['RESOLVER HOST'] as $row) {
+		$adress_resolver = $row;
+		$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
+	    $result1 = file_get_contents($adress_resolver.'peoples/'.$key,false,$context);
+	    $result1 = json_decode($result1, true);
+	    $result = array_merge($result, $result1);
+	}
+	echo json_encode($result); // Envoi de la réponse
+}
+
