635fc418e67f96f2a816186f282b6c2b9bbc3627
Nicoaedhel <nicoa.cn@gmail.com>
2015-03-24 14:10:02 +0100

    Route depot/people
    Ajout de la route depot/people qui décrit les champs requis pour la
    ressource people.

--- core/rsc/depot.php
+++ core/rsc/depot.php
@@ -3,7 +3,8 @@
 $app->get(    '/depot/',                    		'_depot');         		   // affiche information du depot
 $app->get(    '/depot/version/',                    '_depot_version');         // affiche version du depot
 $app->get(    '/depot/option/',                     '_depot_option');          // affiche option du depot
-$app->get(    '/depot/movie/',                      '_depot_movie');          // affiche les champs de la ressource movie
+$app->get(    '/depot/movie/',                      '_depot_movie');           // affiche les champs de la ressource movie
+$app->get(    '/depot/people/',                     '_depot_people');          // affiche les champs de la ressource people
 
 
 function _depot(){
@@ -26,6 +27,12 @@ function _depot_option(){
 
 function _depot_movie(){
 	// Analyse avec sections de core/rsc/movie.ini .
-	$ini_array = parse_ini_file('core/rsc/movie.ini', true);
+	$ini_array = parse_ini_file('core/rsc/people.ini', true);
+	echo json_encode($ini_array); // Envoi de la réponse
+}
+
+function _depot_people(){
+	// Analyse avec sections de core/rsc/movie.ini .
+	$ini_array = parse_ini_file('core/rsc/people.ini', true);
 	echo json_encode($ini_array); // Envoi de la réponse
 }
