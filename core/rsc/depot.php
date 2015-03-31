5bc94fdb79c3cceaca4db0837015e9b5c26cd62d
Nicoaedhel <nicoa.cn@gmail.com>
2015-03-31 11:51:25 +0200

    Refonte de depot.
    Commentaire propre, et fusion des fonctions quand c’est possible.

--- core/rsc/depot.php
+++ core/rsc/depot.php
@@ -4,49 +4,54 @@ $app->get(    '/depot/',                    		'_depot');         		   // affiche
 $app->get(    '/depot/version/',                    '_depot_version');         // affiche version du depot
 $app->get(    '/depot/option/',                     '_depot_option');          // affiche option du depot
 $app->get(    '/depot/resolver/',                   '_depot_resolver');        // affiche tous les depots qui sont connecte à ce depot
-$app->get(    '/depot/movie/',                      '_depot_movie');           // affiche les champs de la ressource movie
-$app->get(    '/depot/people/',                     '_depot_people');          // affiche les champs de la ressource people
-$app->get(    '/depot/organisation/',               '_depot_organisation');    // affiche les champs de la ressource people
+$app->get(    '/depot/resource/:resource/',         '_depot_resource');        // affiche les champs d'une ressource
 
 
 function _depot(){
-	// Analyse avec sections de core/core.ini .
+
+	// Analyse avec sections de depot.ini .
 	$ini_array = parse_ini_file('depot.ini', true);
 	echo json_encode($ini_array); // Envoi de la réponse
+
 }
 
 function _depot_version(){
+
 	// Analyse avec sections de core/core.ini .
 	$ini_array = parse_ini_file('core/core.ini', true);
 	echo json_encode($ini_array['DESCRIPTION']); // Envoi de la réponse
+
 }
 
 function _depot_option(){
-	// Analyse avec sections de core/core.ini .
+
+	// Analyse avec sections de depot.ini .
 	$ini_array = parse_ini_file('depot.ini', true);
 	echo json_encode($ini_array['OPTION']); // Envoi de la réponse
+
 }
 
 function _depot_resolver(){
+
 	// Analyse avec sections de depot.ini .
 	$ini_array = parse_ini_file('depot.ini', true);
 	echo json_encode($ini_array['RESOLVER HOST']); // Envoi de la réponse
+
 }
 
-function _depot_movie(){
-	// Analyse avec sections de core/rsc/movie.ini .
-	$ini_array = parse_ini_file('core/rsc/people.ini', true);
-	echo json_encode($ini_array); // Envoi de la réponse
-}
+function _depot_resource($resource){
 
-function _depot_people(){
-	// Analyse avec sections de core/rsc/movie.ini .
-	$ini_array = parse_ini_file('core/rsc/people.ini', true);
-	echo json_encode($ini_array); // Envoi de la réponse
-}
+	// on créer un variable contenant le chemin vers la ressource
+	$filename = 'core/rsc/'.$resource.'.ini';
 
-function _depot_organisation(){
-	// Analyse avec sections de core/rsc/movie.ini .
-	$ini_array = parse_ini_file('core/rsc/organisation.ini', true);
-	echo json_encode($ini_array); // Envoi de la réponse
+	// on vérifie si le fichier/ressource existe ou non
+	if (file_exists($filename)) { // Si le fichier/ressource existe
+		// Analyse avec sections de core/rsc/'resource'.ini .
+	    $ini_array = parse_ini_file($filename, true);
+	    echo json_encode($ini_array); // Envoi de la réponse 
+	} else { // sinon on renvoi un 404
+	    $app = \Slim\Slim::getInstance();
+	    $app->halt(404);
+	}
+
 }
