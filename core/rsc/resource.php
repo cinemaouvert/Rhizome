10c84f3d83e82dcce4471d9fdc6d1096fe514162
Nicoaedhel <nicoa.cn@gmail.com>
2015-04-02 08:55:19 +0200

    Modification resource.php
    Ajout de la fonction de recherche occurence / valeur et modification du
    listage pour un listage chronologique du plus nouveau au plus ancien
    pour les ressources. Ajout de la date d’édition dans _api_rsc.

--- core/rsc/resource.php
+++ core/rsc/resource.php
@@ -1,8 +1,9 @@
 <?php
 
-$app->get(    '/resource/:resource/',             		 '_resource_list');         	  	  // affiche une liste de ressource du depot
-$app->get(    '/resource/:resource/key/:key/',       	 '_resource_list_by_key');            // affiche une liste de ressource sur le depot via une clé utilisateur
-$app->get(    '/resource/:resource/id/:id/',             '_resource_view');          		  // affiche une resource sur le depot
+$app->get(    '/resource/:resource/',             		 			   '_resource_list');         	  	  	// affiche une liste de ressource du depot
+$app->get(    '/resource/:resource/key/:key/',       	 			   '_resource_list_by_key');            // affiche une liste de ressource sur le depot via une clé utilisateur
+$app->get(    '/resource/:resource/id/:id/',             			   '_resource_view');          		    // affiche une resource sur le depot
+$app->get(    '/resource/:resource/search/:search/:value',             '_resource_list_by_search');         // affiche une liste de resource via une recherche sur le depot
 
 
 function _resource_list($resource){
@@ -18,17 +19,23 @@ function _resource_list($resource){
 	// On verifie si le dossier/ressource existe puis on affiche les informations
 	if(file_exists("depot/$resource")){
 		if($dir = opendir("depot/$resource")){
-			while(false !== ($file = readdir($dir))){
-				if($file != '.' && $file != '..' && $file != '.DS_Store'){
-					$depot = $ini_array['DEPOT']['local'];
-					$id = str_replace('.json','',$file);
-					if($system->_get_http_response_code($depot.'resource/'.$resource.'/id/'.$id) != "404"){
-						$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
-					    $result1 = file_get_contents($depot.'resource/'.$resource.'/id/'.$id,false,$context);
-					    $result1 = json_decode($result1, true);
-					    $result[$i] = $result1;
-					    $i++;
-					}
+			$list_rsc = glob("depot/$resource/*.json");
+			usort($list_rsc, function($a, $b) {
+			    return filemtime($a) < filemtime($b);
+			});
+			foreach($list_rsc as &$value) {
+				$value = str_replace("depot/$resource/", "", $value);
+				$value = str_replace(".json", "", $value);
+			}
+			foreach($list_rsc as &$value) {
+				$depot = $ini_array['DEPOT']['local'];
+				$id = $value;
+				if($system->_get_http_response_code($depot.'resource/'.$resource.'/id/'.$id) != "404"){
+					$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
+				    $result1 = file_get_contents($depot.'resource/'.$resource.'/id/'.$id,false,$context);
+				    $result1 = json_decode($result1, true);
+			    	$result[$i] = $result1;
+			    	$i++;
 				}
 			}
 		}
@@ -60,19 +67,25 @@ function _resource_list_by_key($resource, $key){
 	// On verifie si le dossier/ressource existe puis on affiche les informations
 	if(file_exists("depot/$resource")){
 		if($dir = opendir("depot/$resource")){
-			while(false !== ($file = readdir($dir))){
-				if($file != '.' && $file != '..' && $file != '.DS_Store'){
-					$depot = $ini_array['DEPOT']['local'];
-					$id = str_replace('.json','',$file);
-					if($system->_get_http_response_code($depot.'resource/'.$resource.'/id/'.$id) != "404"){
-						$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
-					    $result1 = file_get_contents($depot.'resource/'.$resource.'/id/'.$id,false,$context);
-					    $result1 = json_decode($result1, true);
-					    if($result1["_api_key_user"] == $key){
-					    	$result[$i] = $result1;
-					    	$i++;
-					    }
-					}
+			$list_rsc = glob("depot/$resource/*.json");
+			usort($list_rsc, function($a, $b) {
+			    return filemtime($a) < filemtime($b);
+			});
+			foreach($list_rsc as &$value) {
+				$value = str_replace("depot/$resource/", "", $value);
+				$value = str_replace(".json", "", $value);
+			}
+			foreach($list_rsc as &$value) {
+				$depot = $ini_array['DEPOT']['local'];
+				$id = $value;
+				if($system->_get_http_response_code($depot.'resource/'.$resource.'/id/'.$id) != "404"){
+					$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
+				    $result1 = file_get_contents($depot.'resource/'.$resource.'/id/'.$id,false,$context);
+				    $result1 = json_decode($result1, true);
+				    if($result1["_api_key_user"] == $key){
+				    	$result[$i] = $result1;
+				    	$i++;
+				    }
 				}
 			}
 		}
@@ -107,6 +120,7 @@ function _resource_view($resource, $id){
 			$result['_api_rsc']['_name'] = $resource;
 			$result['_api_rsc']['_id'] = $id;
 			$result['_api_rsc']['_depot'] = $ini_array['DEPOT']['local'];
+			$result['_api_rsc']['_edited_on'] = date ("F d Y H:i:s.", filemtime("depot/$resource/$id.json"));
 
 			// Lien api
 			$result['_api_link']['_view']['_href'] = $ini_array['DEPOT']['local'].'resource/'.$resource.'/id/'.$result['_api_rsc']['_id'];
@@ -133,7 +147,6 @@ function _resource_view($resource, $id){
 	
 }
 
-
 function _attachment_view($id){
 
 	$ini_array = parse_ini_file('depot/depot.ini', true);
@@ -149,6 +162,67 @@ function _attachment_view($id){
 		$result['data'] = base64_encode(file_get_contents('depot/attachment/'.$id));
 		echo json_encode($result); // Envoi de la réponse
 	} else {
+	    $app = \Slim\Slim::getInstance();
+	    $app->halt(404);
+	}
+	exit(0);
+
+}
+
+function _resource_list_by_search($resource, $search, $value){
+
+	// initialisation des variables et fonctions
+	$value_search = $value;
+	$system = new System();
+	$result = [];
+	$i = 0;
+
+	// Analyse avec sections de depot.ini .
+	$ini_array = parse_ini_file('depot/depot.ini', true);
+
+	// On verifie si le dossier/ressource existe puis on affiche les informations
+	if(file_exists("depot/$resource")){
+		
+		$list_rsc = glob("depot/$resource/*.json");
+		usort($list_rsc, function($a, $b) {
+		    return filemtime($a) < filemtime($b);
+		});
+		foreach($list_rsc as &$value) {
+			$value = str_replace("depot/$resource/", "", $value);
+			$value = str_replace(".json", "", $value);
+		}
+		foreach($list_rsc as &$value) {
+			$depot = $ini_array['DEPOT']['local'];
+			$id = $value;
+			if($system->_get_http_response_code($depot.'resource/'.$resource.'/id/'.$id) != "404"){
+				$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
+			    $result1 = file_get_contents($depot.'resource/'.$resource.'/id/'.$id,false,$context);
+			    $result1 = json_decode($result1, true);
+			    if($result1[$search] == $value_search){
+			    	$result[$i] = $result1;
+			    	$i++;
+			    }
+			    if($result1[$search] == strtoupper($value_search)){
+			    	$result[$i] = $result1;
+			    	$i++;
+			    }
+			    if($result1[$search] == strtolower($value_search)){
+			    	$result[$i] = $result1;
+			    	$i++;
+			    }
+			    if($result1[$search] == ucfirst($value_search)){
+			    	$result[$i] = $result1;
+			    	$i++;
+			    }
+			}
+		}
+		if(isset($result)) echo $system->_filter_json(json_encode($result)); // Envoi de la réponse
+		else {
+			$app = \Slim\Slim::getInstance();
+		    $app->halt(404);
+		}
+	}
+	else {
 	    $app = \Slim\Slim::getInstance();
 	    $app->halt(404);
 	}
