36f71e0dd5b64aefcc6e6e9ab6751deded34b687
Nicoaedhel <nicoa.cn@gmail.com>
2015-04-30 17:19:20 +0200

    Modification echo
    Pour integrer l’affichage de pdf.

--- core/rsc/echo.php
+++ core/rsc/echo.php
@@ -20,18 +20,32 @@ function _echo_attachment_by_id($id){
     	$result1 = json_decode($result1, true);
     	$result = array_merge($result, $result1);
     }
-
-	// On traite le resultat
-	$app->response->headers->set('Content-Type', $result['_api_data']['data_type']);
-	if($result <> null){
-		$data = base64_decode($result['_api_data']['data']);
-		$im = imagecreatefromstring($data);
-		header('Content-Type: '.$result['_api_data']['data_type']);
-	    imagepng($im);
-	    imagedestroy($im);
-	} 
-	else { // s'il n'y a rien alors on fait un 404
-	    $app->halt(404);
-	}
+    if($result['_api_data']['data_type'] == 'image/png' or $result['_api_data']['data_type'] == 'image/jpeg'){
+    	// On traite le resultat
+		$app->response->headers->set('Content-Type', $result['_api_data']['data_type']);
+		if($result <> null){
+			$data = base64_decode($result['_api_data']['data']);
+			$im = imagecreatefromstring($data);
+			header('Content-Type: '.$result['_api_data']['data_type']);
+		    imagepng($im);
+		    imagedestroy($im);
+		} 
+		else { // s'il n'y a rien alors on fait un 404
+		    $app->halt(404);
+		}
+    }
+    elseif($result['_api_data']['data_type'] == 'application/pdf'){
+    	// On traite le resultat
+		$app->response->headers->set('Content-Type', $result['_api_data']['data_type']);
+		if($result <> null){
+			$base64 = $result['_api_data']['data'];
+			$binary = base64_decode($base64);
+			header('Content-type: application/pdf');
+			echo $binary;
+		} 
+		else { // s'il n'y a rien alors on fait un 404
+		    $app->halt(404);
+		}
+    }	
 
 }
